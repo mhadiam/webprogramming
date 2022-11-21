d3c0fb273a76c158be61f28fc46f5e83e52da807
mahdi hadi <mahdihadiam@gmail.com>
2022-11-21 22:45:59 +0330

    trip type input added

--- main.js
+++ main.js
@@ -3,23 +3,52 @@ const to_input = document.querySelector("#to");
 const swap_button = document.querySelector("#swap");
 
 swap_button.addEventListener("click", () => {
-  let x_offset = 0;
+  let y_offset = 0;
   let down = true;
 
   const step = (timestamp) => {
-    from_input.style = `translate: 0px ${x_offset}px`;
-    to_input.style = `translate: 0px ${x_offset}px`;
+    from_input.style = `translate: 0px ${y_offset}px`;
+    to_input.style = `translate: 0px ${y_offset}px`;
 
-    if (x_offset >= 50) {
+    if (y_offset >= 50) {
       down = false;
       let tmp = from_input.value;
       from_input.value = to_input.value;
       to_input.value = tmp;
     }
 
-    down ? (x_offset += 5) : (x_offset -= 5);
+    down ? (y_offset += 5) : (y_offset -= 5);
 
-    if (x_offset >= 0) window.requestAnimationFrame(step);
+    y_offset >= 0 && window.requestAnimationFrame(step);
+  };
+
+  window.requestAnimationFrame(step);
+});
+
+const trip_label = document.querySelector('label[for="trip"]');
+const trip_input = document.querySelector("#trip");
+
+trip_label.addEventListener("click", () => {
+  let y_offset = 0;
+  let down = true;
+
+  const step = (timestamp) => {
+    trip_label.style = `translate: 0px ${y_offset}px`;
+
+    if (y_offset >= 50) {
+      down = false;
+      if (trip_input.value === "one way") {
+        trip_input.setAttribute("value", "return");
+        trip_label.textContent = "رفت و برگشت";
+      } else {
+        trip_input.setAttribute("value", "one way");
+        trip_label.textContent = "یک طرفه";
+      }
+    }
+
+    down ? (y_offset += 5) : (y_offset -= 5);
+
+    y_offset >= 0 && window.requestAnimationFrame(step);
   };
 
   window.requestAnimationFrame(step);
